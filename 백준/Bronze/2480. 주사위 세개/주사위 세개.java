08ddc4ff6d1864d5bc3430867f6ffebafb2be341
Tae90 <xogus54@gmail.com>
2024-08-11 18:33:38 +0900

    [Bronze IV] Title: 주사위 세개, Time: 164 ms, Memory: 17720 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.java
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -5,32 +5,22 @@ public class Main {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-
-		int num[] = new int[3];
-		int A = sc.nextInt();
-		int B = sc.nextInt();
-		int C = sc.nextInt();
-		num[0] = A;
-		num[1] = B;
-		num[2] = C;
-		int max = num[0];
+		int f = sc.nextInt();
+		int s = sc.nextInt();
+		int t = sc.nextInt();
 		int result = 0;
 
-		if (A == B && A == C && B == C) {
-			result = 10000 + (A * 1000);
-		} else if (A == B || A == C) {
-			result = 1000 + (A * 100);
-		} else if (B == A || B == C) {
-			result = 1000 + (B * 100);
+		if (f == s && s == t) {
+			result = 10000 + (f * 1000);
+		} else if (f == s || f == t) {
+			result = 1000 + (f * 100);
+		} else if (s == t) {
+			result = 1000 + (s * 100);
 		} else {
-			for (int i = 0; i < 3; i++) {
-				if (num[i] > max) {
-					max = num[i];
-				}
+			result = ((f > s && f > t) ? f : (s > t) ? s : t) * 100;
 
-			}
-			result = max * 100;
 		}
 		System.out.println(result);
 	}
+
 }
