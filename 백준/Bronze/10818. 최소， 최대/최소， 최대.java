301ebe31cb90583f10ab53de32ae418bd0a07597
Tae90 <xogus54@gmail.com>
2024-08-11 22:11:34 +0900

    [Bronze III] Title: 최소, 최대, Time: 1540 ms, Memory: 318504 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -3,26 +3,24 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+
 		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int[] num = new int[N];
+		int n = sc.nextInt();
+		int number[] = new int[n];
 
-		for (int i = 0; i < N; i++) {
-			num[i] = sc.nextInt();
+		for (int i = 0; i < n; i++) {
+			number[i] = sc.nextInt();
 		}
-
-		int max = num[0];
-		int min = num[0];
-
-		for (int j = 0; j < N; j++) {
-			if (num[j] > max) {
-				max = num[j];
+		int max = number[0];
+		int min = number[0];
+		for (int j = 0; j < n; j++) {
+			if (max < number[j]) {
+				max = number[j];
 			}
-			if (num[j] < min) {
-				min = num[j];
+			if (min > number[j]) {
+				min = number[j];
 			}
 		}
 		System.out.println(min + " " + max);
 	}
-
 }
