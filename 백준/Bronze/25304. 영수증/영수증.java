20cb81cb72ba3a7512a4c12c3e175e1ac192fd86
Tae90 <xogus54@gmail.com>
2024-08-11 18:47:04 +0900

    [Bronze IV] Title: 영수증, Time: 176 ms, Memory: 17772 KB -BaekjoonHub

--- 백준/Bronze/25304. 영수증/영수증.java
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -5,26 +5,20 @@ public class Main {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		int total = sc.nextInt();
+		int x = sc.nextInt();
+		int n = sc.nextInt();
+		int total = 0;
+		for (int i = 0; i < n; i++) {
+			int price = sc.nextInt();
+			int count = sc.nextInt();
+			total += (price * count);
 
-		int number = sc.nextInt();
-
-		int a[] = new int[number];
-		int b[] = new int[number];
-
-		int hap = 0;
-
-		for (int i = 0; i < number; i++) {
-			a[i] = sc.nextInt();
-			b[i] = sc.nextInt();
-			hap += a[i] * b[i];
 		}
-		if (total == hap) {
+		if (x == total) {
 			System.out.println("Yes");
 		} else {
 			System.out.println("No");
 		}
-
 	}
 
 }
