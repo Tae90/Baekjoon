a452b528a3a147aa6777a2101fee34bc67052288
Tae90 <xogus54@gmail.com>
2024-08-11 20:52:37 +0900

    [Bronze V] Title: A+B - 4, Time: 188 ms, Memory: 17892 KB -BaekjoonHub

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -3,14 +3,13 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-	
-		Scanner sc = new Scanner(System.in);
-		
-		while(sc.hasNext()) {
-			int A = sc.nextInt();
-			int B = sc.nextInt();
-			System.out.println(A+B);
+
+		Scanner sc = new Scanner(System.in); 
+
+		while (sc.hasNext()) {
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println(a+b);
 		}
 	}
-
 }
