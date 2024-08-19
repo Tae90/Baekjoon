ac1b4551ee70580af6bc0274fe57b21cb8482f0a
Tae90 <xogus54@gmail.com>
2024-08-19 20:49:59 +0900

    [Bronze V] Title: 아스키 코드, Time: 168 ms, Memory: 17616 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		Scanner sc = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		char C = br.readLine().charAt(0);
+		int num = C;	
+		System.out.println(num);
+		}
+	}
+
